ee2d1d9ca26f75d8fb75e848e3b7ac4efe32be00
Andrew Deal <deal.e.andrew@gmail.com>
2015-11-17 23:32:28 -0800

    Adding solution for Remove Linked List Elements

---
+++ removeLinkedListElements/removeLinkedListElements.js
@@ -0,0 +1,28 @@
+var removeElements = function(head, val) {
+    var prev;
+    if (head === null) return head;
+    if (head.next === null) return head.val === val ? null : head;
+
+    var traverse = function (node) {
+
+     if (node === null) {
+         return head;
+     } else if (node.val === val) {
+        while (node.val === val) {
+            node = node.next;
+            if (node === null) break;
+        }
+        if (prev !== undefined) prev.next = node;
+        else {
+            head = node
+        }
+     }
+     if (node !== null) prev = node;
+     if (node === null) return traverse(node)
+     else {
+         return traverse(node.next);
+     }
+    }
+    return traverse(head);
+
+};

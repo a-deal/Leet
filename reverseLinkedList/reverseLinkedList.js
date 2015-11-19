ede6088fd052a788451318768edb26f8321f85e4
Andrew Deal <deal.e.andrew@gmail.com>
2015-11-18 21:57:07 -0800

    Adding solution for Reverse Linked List

---
+++ reverseLinkedList/reverseLinkedList.js
@@ -0,0 +1,25 @@
+var reverseList = function(head) {
+    var node = head, newHead, newTail;
+
+    if (head === null || head.next === null) return head;
+
+    while (node !== null) {
+        if (node.next === null) {
+            tail.next = node;
+            node = null;
+        }
+        for (var n = node; n !== null; n = n.next) {
+         if (n.next.next === null && newHead === undefined) {
+                newHead = n.next;
+                tail = newHead;
+                n.next = null;
+            } else if (n.next.next === null) {
+                tail.next = n.next;
+                tail = tail.next;
+                n.next = null;
+            }
+        }
+    }
+
+    return newHead;
+};

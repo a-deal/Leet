fb0a7c071352d6b57415a9d3031599a00d6fc711
Andrew Deal <deal.e.andrew@gmail.com>
2015-11-28 13:00:48 -0800

    Refactoring solution for Merge Two Sorted Lists

--- mergeTwoSortedLists/mergeTwoSortedLists.js
+++ mergeTwoSortedLists/mergeTwoSortedLists.js
@@ -1,61 +1,48 @@
-var mergeTwoLists = function(l1, l2) {
-    var l3;
-    var tail;
+var mergeTwoLists = function(l1, l2, l3, tail) {
+   var next;
+    if (l1 === null && l2 === null) {
+        if (l3 === undefined) l3 = null;
+        return l3;
+    } else if (l1 !== null && l2 !== null) {
 
-    function merger (l1, l2) {
-
-        if (l1 === null && l2 === null) {
-            if (l3 === undefined) l3 = null;
-            return;
-        } else if (l1 !== null && l2 !== null) {
-
-            //Find the smaller node
-            if (l1.val < l2.val) {
-                if (l3 === undefined){
-                    l3 = l1;
-                    tail = l3;
-                } else {
-                    tail.next = l1;
-                    tail = tail.next
-                }
-
-                l1 = l1.next;
-            } else {
-                 if (l3 === undefined){
-                    l3 = l2;
-                    tail = l3;
-                } else {
-                    tail.next = l2;
-                    tail = tail.next
-                }
-
-                l2 = l2.next;
-            }
-
-            merger(l1, l2);
-
-        } else if (l1 !== null) {
-            if (l3 === undefined){
-                    l3 = l1;
-            } else {
-              tail.next = l1;
-              l1 = null;
-            }
-
-
-        } else if (l2 !== null) {
-
-           if (l3 === undefined){
-                    l3 = l2;
-            } else {
-              tail.next = l2;
-              l2 = null;
-            }
-
+        //Find the smaller node
+        if (l1.val < l2.val) {
+            next = l1;
+            l1 = l1.next;
+        } else {
+            next = l2;
+            l2 = l2.next;
         }
+
+        if (l3 === undefined){
+            l3 = next;
+            tail = l3;
+        } else {
+            tail.next = next;
+            tail = tail.next
+        }
+
+        return mergeTwoLists(l1, l2, l3, tail);
+
+    } else if (l1 !== null) {
+        if (l3 === undefined){
+                l3 = l1;
+        } else {
+          tail.next = l1;
+          l1 = null;
+        }
+        return l3;
+
+    } else if (l2 !== null) {
+
+       if (l3 === undefined){
+                l3 = l2;
+        } else {
+          tail.next = l2;
+          l2 = null;
+        }
+        return l3;
     }
-    merger(l1,l2);
 
 
-    return l3;
 };

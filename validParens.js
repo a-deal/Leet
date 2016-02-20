bb302e5f4de8e282a944206f20e16c38aa079f3c
Andrew Deal <deal.e.andrew@gmail.com>
2016-02-20 13:01:10 -0800

    Refactors solution for Valid Parens

--- validParens.js
+++ validParens.js
@@ -4,18 +4,25 @@ var pairings = {
     '[' : ']'
 };
 
+var closers = {
+  ')' : ')',
+  '}' : '}',
+  ']' : ']'
+}
+
 var isBalanced = function (str) {
     var stack = [];
     for (var i = 0; i < str.length; i++) {
         if (str[i] in pairings) {
             stack.push(str[i])
-            continue;
-        }
-
-        var top = stack.pop();
-        if (top === undefined || str[i] !== pairings[top]) {
-            return false;
+        } else if (str[i] in closers) {
+          var top = stack.pop();
+          if (top === undefined || str[i] !== pairings[top]) {
+              return false;
+          }
         }
     }
     return stack.length === 0;
 }
+
+console.log(isBalanced('((abk(cdj))'));

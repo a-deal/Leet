dfc7f74998f3b7b469c0b0c599cf953a9f7b12d3
Andrew Deal <deal.e.andrew@gmail.com>
2016-03-02 20:13:31 -0800

    Refactors solution for Inorder Traversal

--- Stacks/inorderTraversalBT.js
+++ Stacks/inorderTraversalBT.js
@@ -16,60 +16,20 @@ for node in T
 return results
 
 
-/////// Code
-
-var inorderTraversal = function(root, results) {
-    results = results || [];
-
-    if (root !== null) {
-        if (root.left !== null)
-            inorderTraversal(root.left, results);
-
-        results.push(root.val);
-
-        if (root.right !== null)
-            inorderTraversal(root.right, results);
-    }
-
-    return results
-};
-
-****** Iteratively ******
-stack = []
-results = []
-
-// Left subtree
-var node = head
-
-while (stack.length > 0)
-    while(node.left !== null)
-        stack.push(node)
-    var temp = stack.pop()
-    results.push(temp.val)
-    if (temp.right !== null)
-        stack.push(node);
-
 */
+
 var inorderTraversal = function(root) {
-    var inorder = [], stack = [root];
-    while (root && stack.length) {
-        var currentRoot = stack[stack.length - 1];
+    var inorder = [], stack = [];
+    while (stack.length || root)
 
-        while(currentRoot) {
-            var left = currentRoot.left;
-            currentRoot.left = null;
-            stack.push(left)
-            currentRoot = left;
-        }
+      if (root)
+        stack.push(root);
+        root = root.left;
+      else
+        temp = stack.pop();
+        inorder.push(temp.val);
+        root = temp.right
 
-        currentRoot = stack.pop();
-        inorder.push(currentRoot.val);
-
-        if (currentRoot.right !== null) {
-            stack.push(currentRoot.right)
-            currentRoot.right = null
-        }
-   }
 
     return inorder;
 };

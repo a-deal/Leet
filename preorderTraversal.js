33f1beb482dd748421cae0d068b950eff0abd899
Andrew Deal <deal.e.andrew@gmail.com>
2016-03-02 18:29:45 -0800

    Refactors solution for preorder traversal

--- preorderTraversal.js
+++ preorderTraversal.js
@@ -3,37 +3,25 @@ Input: <object> root
 Output : <array> preorder
 
 /////////////// PseudoCode ////////////////
-
 function preorderTraversal (root)
-  let preorder = '', stack = [root]
+  let preorder = [], stack = [root];
 
-  while (stack.length)
-    let currentRoot = stack[-1]
+  while (root && stack.length) 
+    var currentRoot = stack.pop();
 
-    preorder += currentRoot.val
+    preorder.push(currentRoot.val);
 
-    while currentRoot
-      let left = currentRoot.left
-      if (left === null)
-        preorder += '#'
-      else
-        preorder += left.val
-        stack.push(left)
+    if (currentRoot.right !== null) stack.push(currentRoot.right);
+
+    while (currentRoot.left)
+      var left = currentRoot.left
+      preorder.push(left.val)
+      if (left.right) stack.push(left.right)
       currentRoot.left = null;
       currentRoot = left;
 
-    currentRoot = stack.pop()
+  return preorder;
 
-    while currentRoot
-      let right = currentRoot.right
-      if (right === null)
-        preorder += '#'
-      else
-        preorder += right.val
-        stack.push(right)
-      currentRoot.right = null;
-      currentRoot = right;
-  return preorder
 
 
 */

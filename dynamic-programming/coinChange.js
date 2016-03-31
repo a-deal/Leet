2b8abe39220e178658e7c9899233bbca9ed33cb9
Andrew Deal <deal.e.andrew@gmail.com>
2016-03-31 08:32:12 -0700

    Adds solution for Coin Change

--- dynamic-programming/coinChange.js
+++ dynamic-programming/coinChange.js
@@ -1,4 +1,5 @@
 /*
+
 You are given coins of different denominations and a total amount of money amount.
 Write a function to compute the fewest number of coins that you need to make up that amount.
 If that amount of money cannot be made up by any combination of the coins, return -1.
@@ -14,60 +15,58 @@ return -1.
 Note:
 You may assume that you have an infinite number of each kind of coin.
 
-Input  : <int> amount, <array> coins
-Output : <int> fewestCoins
+coins = [1,2,5], amount = 11;
 
-Coins = [1,2,3,4,5] , Amount = 11
-
-function coinChange (coins, amount, coinsUsedSoFar, cache) {
-  coinsUsedSoFar = coinsUsedSoFar || 0;
-  cache = cache || {};
-
-  if (amount < 0) return;
-  if (cache[amount]) {
-    if (coinsUsedSoFar < cache[amount]) {
-        cache[amount] = coinsUsedSoFar;
-    } else {
-      return;
-    }
-  }
-  if (!cache[amount]) cache[amount] = coinsUsedSoFar;
-
-  for (var i = 0; i < coins.length; i++) {
-    coinChange(coins, amount - coins[i], coinsUsedSoFar + 1, cache);
-  }
-  return cache[0] || -1;
-}
+coinChange(11) => Math.min(11 - 1, 11 - 2, 11 - 5)
+-------------------
+coinChange(10) => Math.min(10 - 1, 10 - 2, 10 - 5)
+coinChange(9) => Math.min(9 - 1, 9 - 2, 9 - 5)
+coinChange(6) => Math.min(6 - 1, 6 - 2, 6 - 5)
+-------------------
+coinChange(9) => Math.min(9 - 1, 9 - 2, 9 - 5)
+coinChange(8) => Math.min(8 - 1, 8 - 2, 8 - 5)
+coinChange(5) => Math.min(5 - 1, 5 - 2, 5 - 5)
+coinChange(8) => Math.min(8 - 1, 8 - 2, 8 - 5)
+coinChange(7) => Math.min(7 - 1, 7 - 2, 7 - 5)
+coinChange(4) => Math.min(4 - 1, 4 - 2, 4 - 5)
+coinChange(5) => Math.min(5 - 1, 5 - 2, 5 - 5)
+coinChange(4) => Math.min(4 - 1, 4 - 2, 4 - 5)
+coinChange(1) => Math.min(1 - 1, 1 - 2, 1 - 5)
+         0 1 2 3 4 5 6 7 8 9 10 11
+[2,5]   [0,1,1,2,2,1,2,2,3,3,2,3]
 
 */
 "use strict";
-
 function coinChange (coins, amount) {
-  var cache = {}
-  for (var i = 0; i < coins.length; i++)
-    cache[coins[i]] = 1;
-  var smallestCoin = coins.sort((a,b) => a - b)[0];
-  var min = smallestCoin;
-  var coinsUsedSoFar = 1;
-  while (min <= amount && !cache[amount]){
-    min += smallestCoin;
-    coinsUsedSoFar++;
-    var oldCache = Object.assign({}, cache);
-    for (var sum in oldCache) {
-      for (var j = 0; j < coins.length; j++) {
-        if (!oldCache[+sum + coins[j]]){
-          cache[+sum + coins[j]] = coinsUsedSoFar;
-        }
-      }
-    }
+  if(!amount) return 0;
+  var cache = [0]
+  for (var i = 1; i <= amount; i++) {
+    findMin(i, coins, cache);
   }
-  return cache[amount] || -1;
+  return cache[cache.length - 1] === 0 ? -1 : cache[cache.length - 1];
 }
 
-console.log(coinChange([1,3,5], 11)) // 3
-console.log(coinChange([2], 3)) // -1
-console.log(coinChange([1,2,5], 50)); // 10
-console.log(coinChange([1,2,5], 1000)) // 200
-console.log(coinChange([243,291,335,209,177,345,114,91,313,331], 7367)); //23
-console.log(coinChange([313,230,410,263,338,469,431,118,41,221],4906)); //11
-console.log(coinChange([58,92,387,421,194,208,231],7798)); //21
+function findMin (amount, coins, cache) {
+  var min = Infinity;
+  for (var j = 0; j < coins.length; j++) {
+    var remainder = amount - coins[j];
+    if (cache[remainder] === 0 && remainder > 0) continue;
+    if (cache[remainder] < min && remainder >= 0) min = 1 + cache[remainder];
+  }
+  cache[amount] = min === Infinity ? 0 : min;
+}
+
+function assertEquals(expected, actual) {
+  if(expected === actual) {
+    console.log(expected + " === " + actual + " OK");
+  } else {
+    console.log("Expected [" + expected + "] but was [" + actual + "]");
+  }
+}
+assertEquals(0, coinChange([1],0))
+assertEquals(-1, coinChange([2], 3)) // -1
+assertEquals(10, coinChange([1,2,5], 50)); // 10
+assertEquals(200, coinChange([1,2,5], 1000)) // 200
+assertEquals(23, coinChange([243,291,335,209,177,345,114,91,313,331], 7367)); //23
+assertEquals(11, coinChange([313,230,410,263,338,469,431,118,41,221],4906)); //11
+assertEquals(21, coinChange([58,92,387,421,194,208,231],7798)); //21
